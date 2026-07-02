d59a5a6f3dcbb5691236a2e267543e2eb57d0663
shreepalikaruna5-arch <shreepalikaruna5@gmail.com>
2026-07-02 15:21:49 -0700

    Add Question1.java

---
+++ Question1.java
@@ -0,0 +1,8 @@
+//Write a program that declares a variable named `studentName` and prints it.
+public class Question1{
+    public static void main(String[] args){
+        String studentName;
+        studentName = "Karuna";
+        System.out.println("Student name is " + studentName);
+    }
+}
